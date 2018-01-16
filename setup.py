2f866c517c7e2e5b8c058a672652a94653cb6253
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-01-16 14:31:20 +0100

    Back to development: 3.0.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ long_description = (
 
 setup(
     name="hurry.workflow",
-    version='3.0.0',
+    version='3.0.1.dev0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     namespace_packages=['hurry'],
