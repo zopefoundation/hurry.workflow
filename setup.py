374498f1eeadd0aec3551306553d698fa7c77ad3
Marius Gedminas <marius@gedmin.as>
2018-10-19 00:58:55 +0300

    Switch to new Framework :: Zope :: 3 classifier
    See https://github.com/zopefoundation/z3c.authviewlet/pull/6#discussion_r226414566
    for the background behind this.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
-        'Framework :: Zope3',
+        'Framework :: Zope :: 3',
         ],
     extras_require=dict(
         test=[],
