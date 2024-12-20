9f221ec786f757fceb4bc0811f926e9246c98b33
Michael Howitz <icemac@gmx.net>
2024-12-20 09:35:29 +0100

    Preparing release 4.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description = (
 
 setup(
     name="hurry.workflow",
-    version='4.0.dev0',
+    version='4.0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     namespace_packages=['hurry'],
